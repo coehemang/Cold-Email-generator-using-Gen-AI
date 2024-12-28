168de55daf82991c061f6678d6b048cafb344822
Er-Mayank-Aggarwal <mayank1gdsc@gmail.com>
2024-12-28 20:03:04 +0530

    Used DropBox

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import chromadb
 import uuid
+import requests
+from io import StringIO
+
+shared_link = "https://www.dropbox.com/scl/fi/68pp185mpnbzple629h5c/my_portfolio.csv?rlkey=5mgk30vpmo0gc4as0njzs8up5&st=3gtuzx2y&dl=1"
+
+def fetch_csv_from_dropbox_url(url):
+        response = requests.get(url)
+        response.raise_for_status()  
+        csv_data = pd.read_csv(StringIO(response.text))
+        return csv_data  
+
 
 
 class Portfolio:
-    def __init__(self, file_path="app/resources/my_portfolio.csv"):
-        self.file_path = file_path
-        self.data = pd.read_csv(file_path)
+    def __init__(self):
+        self.data = fetch_csv_from_dropbox_url(shared_link)
         self.chroma_client = chromadb.PersistentClient('vectorstore')
         self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
