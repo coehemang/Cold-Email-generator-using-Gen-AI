b50730c9efd827166ac88b7dea188ad6f6c369cd
Hemang Choudhary <hemangc37@gmail.com>
2024-12-29 03:39:36 +0530

    done

--- app/portfolio.py
+++ app/portfolio.py
@@ -10,43 +10,31 @@ shared_link = "https://www.dropbox.com/scl/fi/68pp185mpnbzple629h5c/my_portfolio
 
 
 def fetch_csv_from_dropbox_url(url):
-    """Fetches CSV data from the provided Dropbox URL."""
-    try:
         response = requests.get(url)
         response.raise_for_status()
         csv_data = pd.read_csv(StringIO(response.text))
         return csv_data
-    except requests.exceptions.RequestException as e:
-        raise Exception(f"Error downloading CSV: {e}")
 
 
 class Portfolio:
     def __init__(self):
-        """Initializes the Portfolio class."""
-        # Load portfolio data
         self.data = fetch_csv_from_dropbox_url(shared_link)
-
-        # Initialize Pinecone with new method
         self.pinecone_client = Pinecone(api_key="pcsk_2nQXbL_AmyBNAPz7zz9DwXe3mDr65yFU1rF6EJf9f1vQbZir4SzBLk9DTQBVq5CVDUqRP2")
-        self.index_name = "portfolioindex"  # Ensure the name is lowercase and valid
+        self.index_name = "portfolioindex" 
 
-        # Check if index exists; if not, create it
         if self.index_name not in [index["name"] for index in self.pinecone_client.list_indexes().indexes]:
             self.pinecone_client.create_index(
                 name=self.index_name,
-                dimension=384,  # Ensure this matches the dimensionality of your embeddings
-                metric="cosine",  # Choose the appropriate metric
-                spec=ServerlessSpec(cloud="aws", region="us-east-1")  # Replace with your desired cloud and region
+                dimension=384,  
+                metric="cosine",  
+                spec=ServerlessSpec(cloud="aws", region="us-east-1")  
             )
 
-        # Connect to the index
         self.index = self.pinecone_client.Index(self.index_name)
 
-        # Load the SentenceTransformer model
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
 
     def load_portfolio(self):
-        """Loads and embeds portfolio data into the Pinecone index."""
         # Get index stats
         stats = self.index.describe_index_stats()
 
@@ -66,8 +54,6 @@ class Portfolio:
             self.index.upsert(vectors)
 
     def query_links(self, skills):
-        """Queries the Pinecone index based on skills and returns relevant links."""
-        # Compute the embedding for the query
         query_embedding = self.model.encode(skills).tolist()
 
         # Query the Pinecone index
